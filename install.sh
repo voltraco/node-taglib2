307e7e1d65e47a0cce0793794c43615278adab27
0x00a <paolo@async.ly>
2016-07-13 16:36:47 -0400

    update build items

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-if [[ -n "$ELECTRON" ]]
+if [ -n "$ELECTRON" ]
 then
-  cmake-js rebuild -r electron -v 1.2.5 -a ia64
+  CC=clang CXX=clang++ cmake-js rebuild -r electron -v 1.2.5 -a ia64
 else
   cmake-js compile
 fi
