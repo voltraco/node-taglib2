32b57661fae2d887b8700ca3c4f8201ad668e443
0x00a <paolo@async.ly>
2016-07-15 08:56:24 -0400

    update electron version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if [ -n "$ELECTRON" ]
 then
-  CC=clang CXX=clang++ cmake-js rebuild -r electron -v 1.2.5 -a ia64
+  CC=clang CXX=clang++ cmake-js rebuild -r electron -v 1.2.7 -a ia64
 else
   cmake-js compile
 fi
