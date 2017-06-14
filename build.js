2e0e11c622decf80f736d3e254d72d7b7f0e5a48
Julian Gruber <julian@juliangruber.com>
2017-06-14 22:15:46 +0200

    cmake-js wtf

---
+++ build.js
@@ -0,0 +1,13 @@
+var spawn = require('child_process').spawn
+
+var ps = spawn('cmake-js', [
+  'rebuild',
+  '-r', process.env.npm_config_runtime || 'node',
+  '-v', process.env.npm_config_target || process.versions.node,
+  '--abi', process.env.npm_config_abi || process.versions.modules
+])
+ps.stdout.pipe(process.stdout)
+ps.stderr.pipe(process.stderr)
+ps.on('exit', function (code) {
+  process.exit(code)
+})
