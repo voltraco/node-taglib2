8607b5e6e5ead639e1e2f14dc3df43684fc6e39c
Julian Gruber <julian@juliangruber.com>
2017-10-30 02:17:11 +0100

    fix windows builds using npm-run-path

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 var spawn = require('cross-spawn')
+var npmRunPath = require('npm-run-path')
 
 var runtime = process.env.npm_config_runtime || 'node'
 var target = process.env.npm_config_target || process.versions.node
@@ -6,12 +7,14 @@ var abi = process.env.npm_config_abi || process.versions.modules
 
 console.log('BUILD for %s@%s (abi=%s)', runtime, target, abi)
 
-var ps = spawn(`${__dirname}/node_modules/.bin/cmake-js`, [
+var ps = spawn('cmake-js', [
   'rebuild',
   '-r', runtime,
   '-v', target,
   '--abi', abi
-])
+], {
+  env: npmRunPath.env()
+})
 ps.stdout.pipe(process.stdout)
 ps.stderr.pipe(process.stderr)
 ps.on('exit', function (code) {
